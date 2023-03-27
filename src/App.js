615ce851c484460950f1ff770479f74cee2d8cc2
Igor Stepanov <stipyk1309@gmail.com>
2023-03-27 22:37:29 +0300

    delete unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Main } from "./pages/Main";
 import { CharacterPage } from "./pages/CharacterPage";
 
